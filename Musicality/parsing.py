2b9f45bcad954b4e84c54a3d7fa18a60cccac62d
Roman Igroman <114226705+r0mashka112@users.noreply.github.com>
2023-09-22 00:32:35 +0300

    realize

---
+++ Musicality/parsing.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests, random, config
+
+
+async def getAudio() -> (dict, str):
+    page = requests.get(config.URL + str(random.randint(1, 10)))
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    data = soup.find_all('li', class_ = 'item')
+
+    rand_Item = random.randint(0, 109)
+
+    info = {
+        'artist' : data[rand_Item].find('span', class_ = 'artist').text, 
+        'track' : data[rand_Item].find('span', class_ = 'track').text
+            }
+
+    url = data[rand_Item].find('li', class_ = 'play')['data-url']
+
+    return info, url
+
+
+async def get_list_button_items(class_tag, object) -> list:
+    page = requests.get(config.URL + str(random.randint(1, 10)))
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    data = list(item.text for item in soup.find_all('span', class_ = class_tag))
+
+    unique_data_array = list()
+
+    counter = 0
+
+    while counter < config.QUANTITY_VARIANTS_ANSWER - 1:
+        item = data[random.randint(0, len(data))]
+        if (item not in unique_data_array) and (item != object.SONG_INFO[class_tag]) \
+        and (counter < config.QUANTITY_VARIANTS_ANSWER - 1):
+            unique_data_array.append(item)
+            counter += 1
+
+    return unique_data_array
